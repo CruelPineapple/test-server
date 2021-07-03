74f6c21eae83e355baf15f3d3e1ee3c62753ded3
sakurajimama1 <1617218599@qq.com>
2021-07-03 23:48:05 +0800

    finish insert db and create a interface

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ let express = require('express');
 let bodyParse = require('body-parser')
 
 const mysql = require('mysql');
+
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -22,7 +23,10 @@ connection.query('select * from test_table where test_id ="00000"',(err,results,
         console.log(err);
     }
     console.log(results);
+    console.log("id:"+results[0].test_id);
 })
+
+
 
 let server = express()
 
@@ -40,6 +44,48 @@ server.use(bodyParse.urlencoded({
 server.post('/', function (request, response) {
     //解析post请求参数
     console.log(request.body);
-    response.send(request.body)
+    let uid = request.body.byUID;
+    let t=new Date();
+    let tStr = t.toString();
+    let temp = request.body.byTemper;
+    let hum = request.body.byHum;
+    let queryStr = "INSERT INTO pi_data VALUES ("+"\""+uid+"\",\""+tStr+"\",\""+temp+"\",\""+hum+"\")";
+    connection.query(queryStr,(err,results)=>{
+        if(err){
+            console.log("err:"+err);
+        }
+        console.log("results",results);
+        //let queryRes = results;
+    })
+
+    let resObj={};
+    resObj['req']=request.body;
+    resObj['time']=t;
+    resObj['msg']='success';
+    response.send(resObj);
 })
+
+server.post('//getdata', function (request, response) {
+    //解析post请求参数
+    console.log(request.body);
+    let uid = request.body.uid;
+    let queryData;
+    let queryStr = "select * from pi_data where uid=\""+uid+"\" order by time desc";
+    connection.query(queryStr,(err,results)=>{
+        if(err){
+            console.log("err:"+err);
+        }
+        console.log(results[0]);
+        queryData=results[0];
+        //let queryRes = results;
+        let resObj={};
+        resObj['data']=queryData;
+        resObj['msg']='not fully success';
+        console.log(resObj);
+        response.send(resObj);
+    })
+
+
+})
+
 server.listen(7777);
